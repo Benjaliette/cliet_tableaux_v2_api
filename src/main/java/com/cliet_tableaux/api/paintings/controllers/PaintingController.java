858223b5b7ce83473aa79e8d6c1896d10a7171ba
Benjamin Liet <benjamin.liet.dev@gmail.com>
2024-07-03 15:46:55 +0200

    modified the swagger openapi ui for entities and routes documentation

--- src/main/java/com/cliet_tableaux/api/paintings/controllers/PaintingController.java
+++ src/main/java/com/cliet_tableaux/api/paintings/controllers/PaintingController.java
@@ -2,6 +2,8 @@ package com.cliet_tableaux.api.paintings.controllers;
 
 import com.cliet_tableaux.api.paintings.dtos.PaintingDto;
 import com.cliet_tableaux.api.paintings.services.PaintingService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,20 +13,33 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/paintings")
+@Tag(name = "Paintings API")
 public class PaintingController {
     @Autowired
     private PaintingService paintingService;
 
+    @Operation(
+            summary = "Get all paintings",
+            description = "Returns all paintings"
+    )
     @GetMapping
     public ResponseEntity<List<PaintingDto>> getPaintings() {
         return new ResponseEntity<>(paintingService.findAll(), HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Get a painting by id",
+            description = "Returns a painting as per the id"
+    )
     @GetMapping("/{id}")
     public ResponseEntity<PaintingDto> getPainting(@PathVariable Long id) {
         return new ResponseEntity<>(paintingService.find(id), HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Create a new painting",
+            description = "Returns the newly created painting"
+    )
     @PostMapping
     public ResponseEntity<PaintingDto> createPaintings(@RequestBody PaintingDto paintingDto) {
         return new ResponseEntity<>(paintingService.create(paintingDto),
--- src/main/java/com/cliet_tableaux/api/users/controllers/UserController.java
+++ src/main/java/com/cliet_tableaux/api/users/controllers/UserController.java
@@ -2,6 +2,8 @@ package com.cliet_tableaux.api.users.controllers;
 
 import com.cliet_tableaux.api.users.dtos.UserDto;
 import com.cliet_tableaux.api.users.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,16 +15,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
+@Tag(name = "Users API")
 public class UserController {
     @Autowired
     UserService userService;
 
+    @Operation(
+            summary = "Sign up a new user",
+            description = "Returns the newly created user"
+    )
     @PostMapping("/register")
     public ResponseEntity<UserDto> signUp(@RequestBody UserDto userDto) throws Exception {
         return new ResponseEntity<>(userService.signUpUser(userDto),
                 HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Login an user",
+            description = "Returns the logged user"
+    )
     @PostMapping("/login")
     public ResponseEntity<UserDto> login(@RequestBody UserDto userDto) {
         return new ResponseEntity<>(userService.loginUser(userDto),
